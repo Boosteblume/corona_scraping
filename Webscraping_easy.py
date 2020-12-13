140c6f2218b252e4529f8f659c9a95d2a833d52f
„Ghost911-s“ <maxblum94@yahoo.de>
2020-12-14 00:39:37 +0100

    added the total infected

--- Webscraping_easy.py
+++ Webscraping_easy.py
@@ -2,9 +2,6 @@
 
 import requests
 from bs4 import BeautifulSoup
-import csv
-
-from requests_html import HTMLSession
 
 dict_of_cities = { "Freiburg": r"https://www.corona-in-zahlen.de/landkreise/sk%20freiburg%20i.breisgau/"
 , "Kulmbach": r"https://www.corona-in-zahlen.de/landkreise/lk%20kulmbach/"
@@ -16,7 +13,6 @@ list_of_cities = ["Freiburg", "Kulmbach", "Hamburg", "Sigmaringen"]
 
 
 #choose the city you want the numbers
-
 def choose_city():
     print(list_of_cities)
     print("Choose one of the cities mentioned above" )
@@ -34,14 +30,26 @@ print(dict_of_cities[answered_city])
 print("")
 
 #open the url and scrape for the 7-days-incidenc
-def get_urlcontent():
+def newinfections():
     url = dict_of_cities[answered_city]
     url_content = requests.get(url).text
     soup = BeautifulSoup(url_content, "html.parser")
-
 
     list_of_numbers = soup.find_all("p", class_="card-title")[3].text
     #returns a list of all card-title of the website and i want value no.4
     return list_of_numbers
 
-print(get_urlcontent())
+print("The current 7-day incidence value is "+ newinfections())
+print("")
+
+#here is the scrapping for the total infections
+def totalinfections():
+    url = dict_of_cities[answered_city]
+    url_content = requests.get(url).text
+    soup = BeautifulSoup(url_content, "html.parser")
+    #returns a list of all card-title of the website and i want value no.2
+    list_of_numbers = soup.find_all("p", class_="card-title")[1].text
+    return list_of_numbers
+
+print("The total amount of infections is "+ totalinfections())
+print("")
