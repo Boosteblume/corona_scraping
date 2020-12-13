ea070426b9bc4a173ad510296e8f00610fa95669
„Ghost911-s“ <maxblum94@yahoo.de>
2020-12-13 18:47:16 +0100

    v0.2

--- Webscraping_easy.py
+++ Webscraping_easy.py
@@ -1,14 +1,15 @@
 #Webscraping einfache Variante
 
 import requests
+from bs4 import BeautifulSoup
 import csv
 
 from requests_html import HTMLSession
 
-dict_of_cities = { "Freiburg": "https://www.corona-in-zahlen.de/landkreise/sk%20freiburg%20i.breisgau/"
-, "Kulmbach": "https://www.corona-in-zahlen.de/landkreise/lk%20kulmbach/"
-, "Hamburg": "https://www.corona-in-zahlen.de/landkreise/sk%20hamburg/"
-, "Sigmaringen": "https://www.corona-in-zahlen.de/landkreise/lk%20sigmaringen/" }
+dict_of_cities = { "Freiburg": r"https://www.corona-in-zahlen.de/landkreise/sk%20freiburg%20i.breisgau/"
+, "Kulmbach": r"https://www.corona-in-zahlen.de/landkreise/lk%20kulmbach/"
+, "Hamburg": r"https://www.corona-in-zahlen.de/landkreise/sk%20hamburg/"
+, "Sigmaringen": r"https://www.corona-in-zahlen.de/landkreise/lk%20sigmaringen/" }
 
 list_of_cities = ["Freiburg", "Kulmbach", "Hamburg", "Sigmaringen"]
 
@@ -30,8 +31,13 @@ answered_city = choose_city()
 print("You choose " + answered_city)
 get_the_document()
 
-#now lets open the choosen url in a file
-def get_the_document():
+#open the url and scrape for the 7-days-incidenc
+def ():
     url = dict_of_cities[answered_city]
     session = HTMLSession()
-    url_variable = session.get(url)
+    url_session = session.get(url)
+    
+    if url_session.html.find("p.card-text") == "Neuinfektionen (7-Tage-Inzidenz) ":
+        Neuinfektionen = url_session.html.find()
+
+#beautifulsoup find all 
